a4c63ffe92f46f64987a08a4fa708357ed20ac34
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-18 13:50:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ import pickle
 
 #ex2 = pd.read_csv("ALL_TIME_TWEET_SENTIMENT.csv")
 
-ex3 = pd.read_csv("ALL_TIME_TWEET_SENTIMENT_pt2.csv")
+ex3 = pd.read_csv("ALL_TIME_TWEET_SENTIMENT_pt2.csv", lineterminator='\n')
 #ex3 = ex3.append(ex2)
 
 ex3 = ex3[['movie', 'Datetime', 'Text', 'count_racist', 'count_problematic', 'count_sexist',
