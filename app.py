074c49df9da7791a6c89e9aa6eb20ad98afa980b
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-18 13:36:11 -0700

    Revert "fixy pixie"
    This reverts commit d1a49073465e1dc0b4549361fcd550c7a161aa34.

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ class Graph(dbb.Block):
                         hover_data=["Text"] , color = 'score')
             return  figgy, dif0,fig,figgs,figgz
 
-app = dash.Dash(__name__)
-server = app.server
+app = dash.Dash('Hello World')
+
 fig_names = ex3.movie.unique()
 options=[{'label': x, 'value': x} for x in fig_names]
 data = {
