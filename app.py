c127f3b10ea6d52073f8db4c850965d5dd4b67a6
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-24 16:31:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ class Graph(dbb.Block):
      [self.input(component_id='dropdown2', component_property= 'value')]
         )
         def update_graph(input1,selected_dropdown_value , selected_dropdown_value2):
-            ex3['count '+'{}'.format(input1)] = ex3['Text'].str.count(str(input1))
+            ex3['count '+'{}'.format(input1)] = ex3['Text'].str.count(input1)
             ex33 = ex3[ex3['movie'] == str(selected_dropdown_value)]
             # Creation of query method using parameters
             dif0= px.scatter(ex3, x='Datetime', y = ex3['count_racist'],
