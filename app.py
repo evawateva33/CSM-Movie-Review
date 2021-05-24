2409e29e32ccf3e280148d77f708a836c0761f1b
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-24 11:14:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ class Graph(dbb.Block):
                 placeholder='Select specific movie to search'
             ),
 
-        dcc.Input(id=self.register("input1"), type="text", placeholder="Input word to search",),
+        #dcc.Input(id=self.register("input1"), type="text", placeholder="Input word to search",),
         #dcc.Input(id=self.register("input2"), type="text", placeholder="", debounce=True),
 
      dcc.Dropdown( id =self.register('dropdown2'),
@@ -65,21 +65,22 @@ class Graph(dbb.Block):
             self.output('graph', 'figure'),
             self.output('graph2', 'figure'),
             self.output('graph3', 'figure'),
-            self.input("input1", "value"),
+            #self.input("input1", "value"),
             #self.input("input2", "value"),
             [self.input('dropdown', 'value')],
      [self.input(component_id='dropdown2', component_property= 'value')]
         )
-        def update_graph(input1,selected_dropdown_value , selected_dropdown_value2):
-            ex3['count_custom_word'] = ex3['Text'].str.count(str(input1))
-            ex33 = ex3[ex3['movie'] == str(selected_dropdown_value)]
-            # Creation of query method using parameters
+        def update_graph(selected_dropdown_value , selected_dropdown_value2):
             ex3['Text'] = ex3['Text'].astype(str)
             ex3['Text'] = ex3['Text'].str.wrap(30)
             ex3['Text'] = ex3['Text'].apply(lambda x: x.replace('\n', '<br>'))
+            #ex3['count_custom_word'] = ex3['Text'].str.count(str(input1))
+            ex33 = ex3[ex3['movie'] == str(selected_dropdown_value)]
+            # Creation of query method using parameters
+        
             dif0= px.scatter(ex3, x='Datetime', y = ex3['{}'.format(selected_dropdown_value2)],
                             color='movie')
-            figgs = px.line(ex33, x='Datetime',y = ex33['count_custom_word'],
+            figgs = px.line(ex33, x='Datetime',y = ex33['count_problematic'],
                         hover_data=["Text"])
             figgz = px.line(ex33, x='Datetime', y = ex33['{}'.format(selected_dropdown_value2)],
                         hover_data=["Text"] , color = 'score')
