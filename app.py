3f1860efeae8ed97324881c99196f3ea46ff8444
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-24 10:26:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 
 
-
-ex2 = pd.read_csv("ALL_TIME_TWEET_SENTIMENT.csv",lineterminator='\n')
+ex2 = pd.read_csv("ALL_TIME_TWEET_SENTIMENT.csv", lineterminator='\n')
 ex3 = pd.read_csv("ALL_TIME_TWEET_SENTIMENT_pt2.csv", lineterminator='\n')
 ex3 = ex3.append(ex2)
 ex3 = ex3[['movie', 'Datetime', 'Text', 'count_racist', 'count_problematic', 'count_sexist',
@@ -38,10 +37,11 @@ class Graph(dbb.Block):
             dcc.Dropdown(
                 id=self.register('dropdown'),
                 options=self.data.options,
-                value=self.data.value,
+                value="Breakfast at Tiffany's",
+                placeholder='Select specific movie to search'
             ),
 
-        #dcc.Input(id=self.register("input1"), type="text", placeholder="Input word to search",),
+        dcc.Input(id=self.register("input1"), type="text", placeholder="Input word to search",),
         #dcc.Input(id=self.register("input2"), type="text", placeholder="", debounce=True),
 
      dcc.Dropdown( id =self.register('dropdown2'),
@@ -53,7 +53,7 @@ class Graph(dbb.Block):
             {'label': 'count_stigma', 'value':'count_stigma'},
              {'label': 'count_stereotypes', 'value':'count_stereotypes'},
             ],
-        value = 'count_racist'),
+        value = 'count_racist', placeholder="Select a word to see frequency of mentions"),
         dcc.Graph(id=self.register('graph2')),
         dcc.Graph(id=self.register('graph')),
         dcc.Graph(id=self.register('graph3'))
@@ -62,24 +62,26 @@ class Graph(dbb.Block):
 
     def callbacks(self):
         @self.app.callback(
+
             self.output('graph', 'figure'),
             self.output('graph2', 'figure'),
             self.output('graph3', 'figure'),
-
+            self.input("input1", "value"),
+            #self.input("input2", "value"),
             [self.input('dropdown', 'value')],
      [self.input(component_id='dropdown2', component_property= 'value')]
         )
-        def update_graph(selected_dropdown_value , selected_dropdown_value2):
-            #ex3['count '+'{}'.format(input1)] = ex3['Text'].str.count(str(input1))
+        def update_graph(input1,selected_dropdown_value , selected_dropdown_value2):
+            ex3['count_custom_word'] = ex3['Text'].str.count(str(input1))
             ex33 = ex3[ex3['movie'] == str(selected_dropdown_value)]
             # Creation of query method using parameters
             dif0= px.scatter(ex3, x='Datetime', y = ex3['{}'.format(selected_dropdown_value2)],
                             color='movie')
-            figgs = px.line(ex33, x='Datetime',y = ex33['count_stereotypes'],
+            figgs = px.line(ex33, x='Datetime',y = ex33['count_custom_word'],
                         hover_data=["Text"])
             figgz = px.line(ex33, x='Datetime', y = ex33['{}'.format(selected_dropdown_value2)],
                         hover_data=["Text"] , color = 'score')
-            return   figgz,dif0,figgs
+            return   figgs,dif0,figgz
 
 app = dash.Dash(__name__)
 server = app.server
@@ -102,4 +104,4 @@ for graph in graphs:
     graph.callbacks()
 
 if __name__ == '__main__':
-    app.run_server( port=3333)
+    app.run_server( port=3339)
