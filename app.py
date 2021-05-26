d1fc17eb58c368563edcf2db91c7f61d079860e9
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-26 09:05:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ query = f"""SELECT *
             # return results as a dataframe
 ex3 = pd.read_sql(query, con)
 
-
+ex3 = ex3.to_dict('ex3')
 
 ex3 = ex3[['Movie', 'Datetime', 'Text', 'Count_racist', 'Count_problematic', 'Count_sexist',
        'Count_stereotypes', 'Count_whitewashing', 'Count_stigma', 'Score']]
