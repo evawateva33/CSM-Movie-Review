171a8a8609a28e039ecab624d36306559bbf38df
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-26 10:50:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ ex3 = ex3.to_dict()
 
 ex3 = pd.DataFrame.from_dict(ex3)
 
-ex3['datetime'] =  pd.to_datetime(ex3['datetime'], format='%Y-%m-%d %H:%M:%S %z')
+ex3['datetime'] =  pd.to_datetime(ex3['datetime'], format='%Y-%m-%d %H:%M:%S %Z')
 
 ex3 = ex3.dropna()
 class Graph(dbb.Block):
