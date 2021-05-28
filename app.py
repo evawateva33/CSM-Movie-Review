d5c29fba7aa214871cae6d81e0bed3b42e17c29b
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-05-28 13:22:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -79,11 +79,14 @@ ex3['text'] = ex3['text'].apply(lambda x: x.replace('\n', '<br>'))
 
 class Graph(dbb.Block):
     def layout(self):
-        return html.Div(
+        return html.Div([
+    html.Div(
         className="app-header",
-        html.Div('Welcome to the Common Sennse Media Twitter Review Tool /n IF PAGE DOES NOT LOAD, PRESS "COMMAND+SHIFT+R" and then Refresh the Page, REPEAT if needed'),
-        className="app-header--title")
-
+        children=[
+            html.Div('Welcome to the Common Sennse Media Twitter Review Tool /n IF PAGE DOES NOT LOAD, PRESS "COMMAND+SHIFT+R" and then Refresh the Page, REPEAT if needed',
+             className="app-header--title")
+        ]
+    )
     , html.Div(
         children=html.Div([
             html.H5('How To Use This Tool:'),
