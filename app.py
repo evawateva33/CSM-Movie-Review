8445dc439cabe9cc1b1f1a1f8bfcbaedffe48829
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-06-01 14:21:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,8 @@ ex3 = pd.DataFrame.from_dict(ex3)
 
 
 
+ex3['datetime'] = ex3['datetime'].astype(str)
+ex3['text'] = ex3['text'].astype(str)
 ex3['datetime'] =  pd.to_datetime(ex3['datetime'].str[:18], errors = 'coerce',  format='%Y-%m-%d %H:%M:%S')
 ex3['count_yellowface'] = ex3['text'].str.count('yellowface')
 ex3['count_blackface'] = ex3['text'].str.count('blackface')
