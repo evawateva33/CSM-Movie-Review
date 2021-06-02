abaf7a3279ee9d65ce55ff8c12d21bb98df78378
evawateva33 <51424682+evawateva33@users.noreply.github.com>
2021-06-02 11:41:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ class Graph(dbb.Block):
             ),
 
 
-        dcc.Input(id=self.register("input1"),value = 'appropriation',  type="text", placeholder="Input word to search",),
+        dcc.Input(id=self.register("input1"),value = 'bad',  type="text", placeholder="Input word to search",),
         #dcc.Input(id=self.register("input2"), type="text", placeholder="", debounce=True),
 
      dcc.Dropdown( id =self.register('dropdown2'),
